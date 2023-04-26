d30639b8670336d1037033633eac8fc41c51c5f9
kaspr <daniel@dmskaspr.com>
2023-04-25 20:36:43 -0500

    update

--- js/main.js
+++ js/main.js
@@ -23,6 +23,7 @@ let loopedImages;
 let childrenCount;
 let needed;
 let imgNum = 2;
+let path = location.pathname;
 
 // Add recipients to form
 async function addRecipients(){
@@ -159,6 +160,12 @@ async function updateIndividualInvites(information){
 window.onload = async function(){
 	await preloadImages();
 	await loopBanners();
+
+	if(path.includes(`/interests`)){
+		await prefillForm();
+	}else if(path.includes(`/confirm`)){
+		await prefillPage();
+	}
 }
 
 // Independent functions
@@ -170,7 +177,7 @@ banner && banner.addEventListener(`mouseleave`, async function(e){
 	await loopBanners();
 })
 
-
+// Validate first registration page
 async function validateForm(e){
 	let form = e.parentNode;
 	let formInputs = form.getElementsByTagName(`input`);
@@ -181,75 +188,222 @@ async function validateForm(e){
 	let lastName = formInputs[4];
 	let email = formInputs[5];
 	let phoneNumber = formInputs[6];
-	let newsletter = formInputs[7];
-	
+	let newsletterYes = formInputs[7];
+	let newsletterNo = formInputs[8];
+	let usernameError = document.getElementById(`userName`);
+	let passwordError = document.getElementById(`password`);
+	let passwordVerifyError = document.getElementById(`passwordVerify`);
+	let firstNameError = document.getElementById(`firstName`);
+	let lastNameError = document.getElementById(`lastName`);
+	let emailError = document.getElementById(`email`);
+	let phoneNumberError = document.getElementById(`phoneNumber`);
+	let signUpNewsletterError = document.getElementById(`signUpNewsletter`);
+	let message;
+	let errors = false;
+
 	for(let i = 0; i < 5; i++){
 		let input = formInputs[i];
+		let errorElement = document.getElementById(input.name);
 		if(input.value === ``){
 			let label = ((s = ``) => {
 				for(let k of labels){
 					let labelName = k.innerText.trim().slice(0, -1);
-
+					
 					if(k.htmlFor === input.name){
 						s = labelName;
 					}
 				}
 				return s;
 			})();
-
-			alert(`Error: ${label} is empty!`);
+			
+			
+			message = `Error: ${label} is empty!`;
+			errorElement.innerText = message;
+			errorElement.removeAttribute(`hidden`);
+			errorElement.setAttribute(`style`, `color: red;`)
 			input.focus();
 			return false;
+		}else{
+			errorElement.setAttribute(`hidden`, ``);
 		}
 	}
 
 	if(/[^A-Z0-9]+/gi.test(username.value)){
-		alert(`Error: Username may only contain letters and/or numbers!`);
+		message = `Error: Username may only contain letters and/or numbers!`;
+		usernameError.innerText = message;
+		usernameError.removeAttribute(`hidden`);
+		usernameError.setAttribute(`style`, `color: red;`)
 		username.focus();
-		return false;
+		errors = true;
+	}else{
+		usernameError.setAttribute(`hidden`, ``);
 	}
 
 	if(password.value !== passwordVerify.value){
-		alert(`Error: Passwords do not match!`);
+		message = `Error: Passwords do not match!`;
+		passwordVerifyError.innerText = message;
+		passwordVerifyError.removeAttribute(`hidden`);
+		passwordVerifyError.setAttribute(`style`, `color: red;`)
 		passwordVerify.focus();
-		return false;
+		errors = true;
+	}else{
+		passwordVerifyError.setAttribute(`hidden`, ``);
 	}
 
 	if(password.value.length <= 7 || passwordVerify.value.length <= 7){
-		alert(`Error: Passwords must be at least 8 characters in length!`);
+		message = `Error: Passwords must be at least 8 characters in length!`;
+		passwordError.innerText = message;
+		passwordError.removeAttribute(`hidden`);
+		passwordError.setAttribute(`style`, `color: red;`)
 		password.focus();
-		return false;
+		errors = true;
+	}else{
+		passwordError.setAttribute(`hidden`, ``);
 	}
 
 	if(/[^A-Z]+/gi.test(firstName.value)){
-		alert(`Error: First Name may only contain letters!`);
+		message = `Error: First Name may only contain letters!`;
+		firstNameError.innerText = message;
+		firstNameError.removeAttribute(`hidden`);
+		firstNameError.setAttribute(`style`, `color: red;`)
 		firstName.focus();
-		return false;
+		errors = true;
+	}else{
+		firstNameError.setAttribute(`hidden`, ``);
 	}
 
 	if(/[^A-Z]+/gi.test(lastName.value)){
-		alert(`Error: First Name may only contain letters!`);
+		message = `Error: Last Name may only contain letters!`;
+		lastNameError.innerText = message;
+		lastNameError.removeAttribute(`hidden`);
+		lastNameError.setAttribute(`style`, `color: red;`)
 		lastName.focus();
-		return false;
+		errors = true;
+	}else{
+		lastNameError.setAttribute(`hidden`, ``);
 	}
 	
-	if(!/^[A-Z0-9]+@[A-Z0-9.]+\.[A-Z]{3}/gi.test(email.value)){
-		alert(`Error: Email must be in xxx@xxx.xx or xxx@xxx.xxx format!`);
+	if(email.value && !/^[A-Z0-9]+@[A-Z0-9.]+\.[A-Z]{3}/gi.test(email.value)){
+		message = `Error: Email must be in xxx@xxx.xxx format!`;
+		emailError.innerText = message;
+		emailError.removeAttribute(`hidden`);
+		emailError.setAttribute(`style`, `color: red;`)
 		email.focus();
-		return false;
+		errors = true;
+	}else{
+		emailError.setAttribute(`hidden`, ``);
 	}
 
-	if(!/^\(\d{3}\)\ \d{3}\-\d{4}/g.test(phoneNumber.value)){
-		alert(`Error: Phone number must be numbers in (xxx) xxx-xxxx format!`);
+	if(phoneNumber.value && !/^\(\d{3}\)\ \d{3}\-\d{4}/g.test(phoneNumber.value)){
+		message = `Error: Phone number must be numbers in (xxx) xxx-xxxx format!`;
+		phoneNumberError.innerText = message;
+		phoneNumberError.removeAttribute(`hidden`);
+		phoneNumberError.setAttribute(`style`, `color: red;`)
 		phoneNumber.focus();
-		return false;
+		errors = true;
+	}else{
+		phoneNumberError.setAttribute(`hidden`, ``);
 	}
 
-	if(!newsletter.value){
-		alert(`Error: Sign up for newsletter must be Yes/No!`);
-		newsletter.focus();
-		return false;
+	if(!newsletterYes.checked && !newsletterNo.checked){
+		message = `Error: Please select Yes/No`;
+		signUpNewsletterError.innerText = message;
+		signUpNewsletterError.removeAttribute(`hidden`);
+		signUpNewsletterError.setAttribute(`style`, `color: red;`)
+		errors = true;
+	}else{
+		signUpNewsletterError.setAttribute(`hidden`, ``);
 	}
 
-	form.submit();
+	let formData = {
+		username: username.value,
+		password: password.value,
+		passwordVerify: passwordVerify.value,
+		firstName: firstName.value,
+		lastName: lastName.value,
+		email: email.value,
+		phoneNumber: phoneNumber.value,
+		signUpNewsletter: newsletterYes.checked ? `Yes` : `No`,
+	};
+
+	// Create cookies
+	for(let k in formData){
+		document.cookie = `${k.trim()}=${formData[k.trim()]}`;
+	}
+
+	location.href = `./interests.html`;
+}
+
+// Prefill 2nd page of registration
+async function prefillForm(){
+	let cookies = document.cookie.split(`; `);
+	let inputs = document.getElementsByTagName(`input`);
+
+	for(let i = 0; i < cookies.length; i++){
+		let cookie = cookies[i];
+		let cookieName = cookie.split(`=`)[0];
+		let cookieValue = cookie.split(`=`)[1];
+
+		for(let i = 0; i < inputs.length; i++){
+			let input = inputs[i];
+
+			if(input.name === `signUpNewsletter`){
+				if(cookieValue === `Yes`){
+					input.value === `Yes` ? input.setAttribute(`checked`, ``) : 0;
+					input.value === `No` ? input.removeAttribute(`checked`) : 0;
+				}else{
+					input.value === `No` ? input.setAttribute(`checked`, ``) : 0;
+					input.value === `Yes` ? input.removeAttribute(`checked`) : 0;
+				}
+			}else if(input.name === cookieName){
+				input.value = cookieValue;
+			}
+		}
+	}
+}
+
+// Validate 2nd page form
+async function validateForm2(e){
+	let form = e.parentNode;
+	let formInputs = form.getElementsByTagName(`input`);
+	let comments = form.getElementsByTagName(`textarea`)[0];
+	let newsletterYes = formInputs[15];
+	let interests = [];
+
+	console.log(`asdf`,newsletterYes);
+
+	document.cookie = `comments=${comments.value}`;
+	document.cookie = `signUpNewsletter=${newsletterYes.checked ? `Yes` : `No`}`;
+	
+	for(let i = 0; i < formInputs.length; i++){
+		let input = formInputs[i];
+		
+		if(input.type === `checkbox`){
+			input.checked ? interests.push(input.name) : 0;
+		}else if(input.type !== `hidden` && input.type !== `button` && input.name !== `signUpNewsletter`){
+			
+			document.cookie = `${input.name}=${input.value}`;
+		}
+	}
+	
+	document.cookie = `interests=${interests.toString()}`;
+
+	location.href = `./confirm.html`;
+}
+
+// FIll confirmation page with cookie data
+async function prefillPage(){
+	let cookies = document.cookie.split(`; `);
+	let registrationInformation = document.getElementById(`registrationInformation`);
+	let data = ``;
+
+	for(let i = 0; i < cookies.length; i++){
+		let cookie = cookies[i];
+		let cookieName = cookie.split(`=`)[0];
+		let cookieValue = cookie.split(`=`)[1];
+
+		data += `${cookieName}: ${cookieValue}\n\n`;
+	}
+
+	registrationInformation.innerText = data;
 }
